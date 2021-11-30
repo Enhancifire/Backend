d32ec883f1e26453c590c1310b19819e034d16fc
Himanshu Ramteke <hsramteke21@gmail.com>
2021-11-30 22:04:37 +0530

    Connection successful with db

--- sql_connector.py
+++ sql_connector.py
@@ -1,15 +1,25 @@
-def SignUp(self, email, password, username):
+import mysql.connector as c
+con = c.connect(host="localhost", user="root", port="3306",
+                password="root@123", database="BLOG")
+print(con)
+
+
+def SignUp(email, password, username):
     # if user exists, return error
+
     if 1 == 1:
-      return "user exists"
+        return "user exists"
 
-def LoginWithEmail(self, email, password):
+
+def LoginWithEmail(email, password):
     return "UserID"
 
-def LoginWithUsername(self, username, password):
+
+def LoginWithUsername(username, password):
     return "UserID"
 
-def PostList(self):
+
+def PostList():
     return [{
             "postTitle": "postTitle",
             "postId": "postId",
@@ -17,19 +27,23 @@ def PostList(self):
             "dateCreated": "dateCreated"
             }]
 
-def AddPost(self, postTitle, userId, postBody):
+
+def AddPost(postTitle, userId, postBody):
     return "Created Successfully"
 
-def ReturnPost(self, postId):
-    return {
-            "postTitle": "postTitle",
-            "postId": "postId",
-            "postBody": "postBody",
-            "dateCreated": "dateCreated"
-            }
 
-def DeletePost(self, postId):
+def ReturnPost(postId):
+    return {
+        "postTitle": "postTitle",
+        "postId": "postId",
+        "postBody": "postBody",
+        "dateCreated": "dateCreated"
+    }
+
+
+def DeletePost(postId):
     return "Deleted Successfully"
 
-def ModifyPost(self, postId, postBody, postTitle):
+
+def ModifyPost(postId, postBody, postTitle):
     return "Post Modified Successfully"
